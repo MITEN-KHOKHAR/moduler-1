b80ca2f2089a80e78706cf1a8e12004b4a5705ec
MITEN-KHOKHAR <112924423+MITEN-KHOKHAR@users.noreply.github.com>
2023-01-04 10:30:59 +0530

    Delete Project-4 (Bootstrap) Website Sample directory

--- Project-4 (Bootstrap) Website Sample/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["email"]);
-session_destroy();
-echo "<h3 align='center' style='color:red'>You Are Successfully Logout</h3>";
-header('refresh:3,index.php');
-
-?>
